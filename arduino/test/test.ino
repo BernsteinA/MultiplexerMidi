c9fd32e788c2f3047d77e15285b5b8c7b8919156
Aaron Bernstein <aaron@stitchmedia.ca>
2017-12-26 16:29:29 -0500

    updated test.ino with new aftertouch behaviour. this should work without a multiplexer on pin A0. untested

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,5 +1,27 @@
+#define WHICH_SENSOR 0
+#define MAX_SENSOR_VALUE 850
+#define MIN_SENSOR_VALUE 0
+#define NUMBER_OF_SENSORS 16
+#define BASE_NOTE 60
+int currentPressure;
+int noteOnPressure;
+byte currentVelocity;
+byte currentAftertouch;
+bool isOn;
+bool wasOn;
+bool newOn;
+
+byte getVelocity(int sensorValue) {
+   byte outVal = map(sensorValue, MIN_SENSOR_VALUE, MAX_SENSOR_VALUE, 0, 127);
+   return constrain(outVal, 0, 127);
+}
+
+byte getAfterTouch(int sensorValue, int noteOnSensorValue) {
+   byte outVal = map(sensorValue, noteOnSensorValue, MAX_SENSOR_VALUE, 0, 127);
+   return constrain(outVal, 0, 127);
+}
+
 int fsrAnalogPin = 0; 
-int muxValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
 
 int CONTROL0 = 10;
 int CONTROL1 = 11;
@@ -32,19 +54,32 @@ void setup(void)
 
 void loop(void)
 {
-  for (int i = 0; i < 16; i++)
-  {
-    setPin(i);
+    wasOn = isOn;
+    setPin(WHICH_SENSOR);
     delayMicroseconds(50); 
-    muxValues[i] = analogRead(fsrAnalogPin);
-  }
+    currentPressure = analogRead(fsrAnalogPin);
+    
+    newOn = currentPressure >= MIN_SENSOR_VALUE;
 
-  for (int i = 0; i < 15; i++)
-  {
-    Serial.print(muxValues[i]);
+    if(newOn && wasOn) { // aftertouch
+        currentAftertouch = getAfterTouch(currentPressure, noteOnPressure);
+    }
+    else if(!wasOn && newOn) {
+        noteOnPressure = currentPressure;
+        currentVelocity = getVelocity(noteOnPressure);
+    }
+    else if (wasOn && !newOn) {
+        currentVelocity = getVelocity(currentPressure);
+    }
+   
+    isOn = newOn;
+    
+    Serial.print(currentPressure);
     Serial.print(',');
-  }
-
-  Serial.println(muxValues[15]);
-  
+    Serial.print(currentVelocity);
+    Serial.print(',');
+    Serial.print(currentAftertouch);
+    Serial.print(',');
+    Serial.println(isOn);
+    
 }
